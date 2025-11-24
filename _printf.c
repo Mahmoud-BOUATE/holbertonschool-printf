ca201451387c803d671907ad63bb8ec5daa653d1
mahmoud BOUATE <12065@holbertonschool.com>
2025-11-24 14:57:52 +0000

    feat implémentation de la focntion printf et ajout de test

---
+++ _printf.c
@@ -0,0 +1,81 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+#include "main.h"
+/**
+*/
+int _putstr(const char *txt)
+{
+int i, count;
+
+while (txt[i] != '\0')
+{
+    write(1,&txt[i],1);
+    count++;
+    i++;
+}
+return (count);
+}
+/**
+*/
+int _printf(const char *format, ...)
+{
+int i = 0;
+int count = 0;
+char c;
+char *str;
+
+va_list args;
+va_start(args,format);
+
+if (format == NULL)
+return (-1);
+
+
+
+while (format[i] != '\0')
+{
+    if (format[i] == '%')
+    {
+    i++;   
+    if (format[i] == '\0')
+    break;
+     if (format[i] == '%')
+    {
+    write(1, "%", 1);
+    count++;
+    }
+    else if (format[i] == 'c')
+    {
+        c = va_arg(args, int);
+        write(1, &c, 1);
+        count++;
+    }
+    else if (format[i] == 's')
+    {
+        str = va_arg(args, char *);
+        count+= _putstr(str);
+    }
+    else
+    {
+    /* comportement simple pour un spec inconnu : afficher '%' puis le char */
+    write(1, "%", 1);
+    write(1, &format[i], 1);
+    count += 2;
+    }
+    i++; /* on a consommé le caractère de format */
+    continue;
+    }
+    
+    write(1, &format[i] ,1);
+    count ++;
+    i++;
+    }
+    va_end(args);
+    return (count);
+}
+
+
+
+
+
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_PRINTF
+#define FUNCTION_PRINTF
+
+
+int _putstr(const char *txt);
+int _printf(const char *format, ...);
+#endif /* FUNCTION_PRINTF*/
