eb86304ae4a0277b39d97e59956a5f6dded6b26e
Thorgalix <lucaspodevin@sfr.fr>
2025-11-24 17:00:30 +0100

    formatage du code et amélioration de la lisibilité dans _putstr et _printf

--- _printf.c
+++ _printf.c
@@ -8,14 +8,15 @@ int _putstr(const char *txt)
 {
 int i = 0;
 int count = 0;
-if (!txt)
-txt = "(null)";
-while (txt[i] != '\0')
-{
-    write(1,&txt[i],1);
-    i++;
-    count++;
-}
+
+	if (!txt)
+		txt = "(null)";
+	while (txt[i] != '\0')
+	{
+		write(1, &txt[i], 1);
+		i++;
+		count++;
+	}
 return (count);
 }
 /**
@@ -33,55 +34,47 @@ va_list args;
 if (format == NULL)
 return (-1);
 
-va_start(args,format);
+va_start(args, format);
+
 while (format[i] != '\0')
-{
-    
-    if (format[i] == '%')
-    {
-    i++;   
-    if (format[i] == '\0')
-    {
-    va_end(args);
-    return (-1);
-    }
-    
-     if (format[i] == '%')
-    {
-    write(1, "%", 1);
-    count++;
-    }
-    else if (format[i] == 'c')
-    {
-        c = va_arg(args, int);
-        write(1, &c, 1);
-        count++;
-    }
-    else if (format[i] == 's')
-    {
-        str = va_arg(args, char *);
-        count+= _putstr(str);
-    }
-    
-    else
-    {
-    write(1, "%", 1);
-    write(1, &format[i], 1);
-    count += 2;
-    }
-    i++;
-    continue;
-    }
-    
-    write(1, &format[i] ,1);
-    count ++;
-    i++;
-    }
-    va_end(args);
-    return (count);
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				count += _putstr(str);
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		i++;
+		continue;
+		}
+	write(1, &format[i] ,1);
+	count ++;
+	i++;
+	}
+va_end(args);
+return (count);
 }
-
-
-
-
-
