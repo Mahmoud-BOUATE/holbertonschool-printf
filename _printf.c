a6a33f51b3736792332b599ef4abea3b35ee19ee
Thorgalix <lucaspodevin@sfr.fr>
2025-11-25 09:22:51 +0100

    amélioration de la documentation et réorganisation du code dans _putstr et _printf

--- _printf.c
+++ _printf.c
@@ -3,7 +3,10 @@
 #include <unistd.h>
 #include "main.h"
 /**
-*/
+ * _putstr - helper function to print a string
+ * @txt: string to print
+ * Return: number of characters printed
+ */
 int _putstr(const char *txt)
 {
 int i = 0;
@@ -20,7 +23,11 @@ int count = 0;
 return (count);
 }
 /**
-*/
+ * _printf - custom implementation of printf
+ * @format: format string
+ * Return: number of characters printed
+ * Description: This function handles %c, %s, and %% format specifiers.
+ */
 int _printf(const char *format, ...)
 {
 int i = 0;
@@ -37,44 +44,44 @@ return (-1);
 va_start(args, format);
 
 while (format[i] != '\0')
+{
+	if (format[i] == '%')
 	{
+		i++;
+		if (format[i] == '\0')
+		{
+			va_end(args);
+			return (-1);
+		}
 		if (format[i] == '%')
 		{
-			i++;
-			if (format[i] == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			if (format[i] == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
-			else if (format[i] == 'c')
-			{
-				c = va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-			}
-			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				count += _putstr(str);
-			}
-			else
-			{
-				write(1, "%", 1);
-				write(1, &format[i], 1);
-				count += 2;
-			}
+			write(1, "%", 1);
+			count++;
+		}
+		else if (format[i] == 'c')
+		{
+			c = va_arg(args, int);
+			write(1, &c, 1);
+			count++;
+		}
+		else if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			count += _putstr(str);
+		}
+		else
+		{
+			write(1, "%", 1);
+			write(1, &format[i], 1);
+			count += 2;
+		}
 		i++;
 		continue;
-		}
-	write(1, &format[i] ,1);
-	count ++;
-	i++;
 	}
+	write(1, &format[i], 1);
+	count++;
+	i++;
+}
 va_end(args);
 return (count);
 }
